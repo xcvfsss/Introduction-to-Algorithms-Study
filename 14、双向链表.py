0c2788f838a30919b9e77bdf5201776111cc91c7
xcvfsss <xcvfssss@outlook.com>
2023-04-05 01:06:56 +0900

    20230404

--- 14、双向链表.py
+++ 14、双向链表.py
@@ -4,6 +4,7 @@ class ListNode:
         self.prev = None
         self.next = None
 
+
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
@@ -12,16 +13,8 @@ class DoublyLinkedList:
     def is_empty(self):
         return self.head is None
 
-    def size(self):
-        current = self.head
-        count = 0
-        while current:
-            count += 1
-            current = current.next
-        return count
-
-    def append(self, index):
-        new_node = ListNode(index)
+    def append(self, value):
+        new_node = ListNode(value)
         if self.is_empty():
             self.head = self.tail = new_node
         else:
@@ -29,28 +22,30 @@ class DoublyLinkedList:
             self.tail.next = new_node
             self.tail = new_node
 
-    def prepend(self, index):
-        new_node = ListNode(index)
+    def insert(self, position, value):
+        if position <= 0:
+            # 返回的是个什么东西，待会问一下chatgpt
+            return self.prepend(value)
+        elif position >= self.size():
+            return self.append(value)
+
+        new_node = ListNode(value)
+        current = self.head
+        for _ in range(position - 1):
+            current = current.next
+        new_node.prev = current
+        new_node.next = current.next
+        current.next.prev = new_node
+        current.next = new_node
+
+    def prepend(self, value):
+        new_node = ListNode(value)
         if self.is_empty():
             self.head = self.tail = new_node
         else:
             new_node.next = self.head
             self.head.prev = new_node
             self.head = new_node
-
-    def insert(self, position, index):
-        if position <= 0:
-            return self.prepend(index)
-        elif position >= self.size():
-            return self.append(index)
-        new_node = ListNode(index)
-        current = self.head
-        for _ in range(position - 1):
-            current = current.next
-        new_node.next = current.next
-        new_node.prev = current
-        current.next.prev = new_node
-        current.next = new_node
 
     def remove(self, value):
         current = self.head
@@ -67,6 +62,14 @@ class DoublyLinkedList:
                 return
             current = current.next
 
+    def size(self):
+        count = 0
+        current = self.head
+        while current:
+            count += 1
+            current = current.next
+        return count
+
     def __str__(self):
         values = []
         current = self.head
@@ -74,6 +77,7 @@ class DoublyLinkedList:
             values.append(current.value)
             current = current.next
         return ' <-> '.join(str(x) for x in values)
+
 
 if __name__ == '__main__':
     dll = DoublyLinkedList()
